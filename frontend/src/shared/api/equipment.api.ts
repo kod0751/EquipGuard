beefa2a676481506f915233912e2f971e1f33cdb
kod0751 <50727457+kod0751@users.noreply.github.com>
2026-01-06 01:08:07 +0900

    feat(api): 설비 추가 mutation 설정

--- frontend/src/shared/api/equipment.api.ts
+++ frontend/src/shared/api/equipment.api.ts
@@ -1,10 +1,17 @@
 import type { Equipment } from '@/shared/types/equipment';
 import { equipmentMockData } from '../constants/equipment-mock';
+import type { EquipmentFormValues } from '@/features/equipment/schemas/equipment-schema';
 
 export const equipmentApi = {
   getList: async (): Promise<Equipment[]> => {
     // mock이라 delay 흉내
     await new Promise((r) => setTimeout(r, 500));
     return equipmentMockData;
+  },
+  create: async (data: EquipmentFormValues): Promise<void> => {
+    await new Promise((r) => setTimeout(r, 500)); // API 지연 시뮬레이션
+    console.log('API 서버로 데이터 전송 완료:', data);
+    
+    // await axios.post('/api/equipment', data);
   },
 };
--- frontend/src/shared/api/equipment.query.ts
+++ frontend/src/shared/api/equipment.query.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { equipmentApi } from './equipment.api';
 import { equipmentKeys } from './equipment.keys';
 
@@ -7,3 +7,15 @@ export const useEquipmentListQuery = () =>
     queryKey: equipmentKeys.list(),
     queryFn: equipmentApi.getList,
   });
+
+export const useCreateEquipmentMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: equipmentApi.create,
+    onSuccess: () => {
+      // 성공 시 'equipment' 리스트 데이터를 최신화하기 위해 캐시 무효화
+      queryClient.invalidateQueries({ queryKey: equipmentKeys.list() });
+    },
+  });
+};
